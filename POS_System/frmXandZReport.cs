aa34180b70976497a7b37d636011fd6f053aec4b
Rokuseru <barceroxellecleve@gmail.com>
2022-08-08 18:55:51 +0800

    Update New Form and Codes

---
+++ POS_System/frmXandZReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CapstoneProject_3.POS_System
+{
+    public partial class frmXandZReport : Form
+    {
+        private string con = System.Configuration.ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
+        public frmXandZReport()
+        {
+            InitializeComponent();
+        }
+        public void loadXReport()
+        {
+
+        }
+    }
+}
